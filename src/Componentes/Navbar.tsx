5b3295b0bdd30e338515e4f3fbdab6880d81512c
josy12 <josmer1997@hotmail.es>
2025-04-01 14:26:42 -0500

    ajustes login

--- src/Componentes/Navbar.tsx
+++ src/Componentes/Navbar.tsx
@@ -25,64 +25,72 @@ const Navbar = () => {
               <ul className='hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12'>
                 <li>
                   <a className='hover:text-gray-200' href='#'>
-                    Home
+                    Inicio
                   </a>
                 </li>
                 <li>
                   <a className='hover:text-gray-200' href='#'>
-                    Catagory
+                    Categorias
                   </a>
                 </li>
                 <li>
                   <a className='hover:text-gray-200' href='#'>
-                    Collections
+                    Productos
                   </a>
                 </li>
                 <li>
                   <a className='hover:text-gray-200' href='#'>
-                    Contact Us
+                    Contacto
                   </a>
                 </li>
               </ul>
-              {/* // aqui comienzan mis favoritos de usuarios */}
               <div className='hidden xl:flex   space-x-5 items-center'>
-                <a className='hover:text-gray-200' href='#'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-                    />
-                  </svg>
-                </a>
-                {/* aqui comienza mis carrito de compras de usuarios */}
-                <a className='flex items-center hover:text-gray-200' href='#'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-                    />
-                  </svg>
-                  <span className='flex absolute -mt-5 ml-4'>
-                    <span className='animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75'></span>
-                    <span className='relative inline-flex rounded-full h-3 w-3 bg-pink-500'></span>
-                  </span>
-                </a>
+                {/* // aqui comienzan mis favoritos de usuarios */}
+                {isAuthenticated && (
+                  <>
+                    <a className='hover:text-gray-200' href='#'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+                        />
+                      </svg>
+                    </a>
+                    {/* aqui comienza mis carrito de compras de usuarios */}
+                    <a
+                      className='flex items-center hover:text-gray-200'
+                      href='#'
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                        />
+                      </svg>
+                      <span className='flex absolute -mt-5 ml-4'>
+                        <span className='animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-400 opacity-75'></span>
+                        <span className='relative inline-flex rounded-full h-3 w-3 bg-green-500'></span>
+                      </span>
+                    </a>
+                  </>
+                )}
+
                 {/* aqui comienza mi perfil de usuario  */}
                 <a
                   className='flex items-center hover:text-gray-200'
@@ -133,32 +141,31 @@ const Navbar = () => {
                     <span className='relative inline-flex rounded-full h-3 w-3 bg-green-500'></span>
                   </span>
                 </a>
-                {/* barra burger de navbar para resolucion mobil */}
-
-                <a
-                  onClick={() => {
-                    Setsidbar(!sidebar)
-                  }}
-                  className='navbar-burger self-center mr-12 xl:hidden'
-                  href='#'
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6 hover:text-gray-200'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M4 6h16M4 12h16M4 18h16'
-                    />
-                  </svg>
-                </a>
               </>
             )}
+            {/* barra burger de navbar para resolucion mobil */}
+
+            <a
+              onClick={() => {
+                Setsidbar(!sidebar)
+              }}
+              className='navbar-burger self-center mr-12 xl:hidden'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6 hover:text-gray-200'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            </a>
 
             {sidebar && <Sidebar />}
           </nav>
